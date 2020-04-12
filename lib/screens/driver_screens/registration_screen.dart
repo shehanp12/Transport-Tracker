21135a248b22de38cf24f5a966fc8af06b2d4e97
shehan <shehanp068@gmail.com>
2020-04-12 09:20:28 +0530

    ROUTING

---
+++ lib/screens/driver_screens/registration_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import '../../utils/firebase_auth.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = 'registration_screen';
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+ TextEditingController _emailController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController(text: "");
+    _passwordController = TextEditingController(text: "");
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 100.0),
+              Text("Sign up", style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0
+              ),),
+              const SizedBox(height: 20.0),
+              
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                    hintText: "Enter email"
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter password"
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              RaisedButton(
+                child: Text("Sign up"),
+                onPressed: ()async {
+                  if(_emailController.text.isEmpty || _passwordController.text.isEmpty) {
+                    print("Email and password cannot be empty");
+                    return;
+                  }
+                  bool res = await AuthProvider().registerWithEmailAndPassword(_emailController.text, _passwordController.text);
+                  if(!res) {
+                    print("Login failed");
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/driver_screens/welcome_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+import 'registration_screen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                 
+                  height: 60.0,
+                ),
+                Text(
+                  'Driver Login',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+             Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: ()  {
+                     Navigator.pushNamed(context, LoginScreen.id);
+                    
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Sign In ',
+                  ),
+                )
+              ),
+            ),
+             Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: ()  {
+                     Navigator.pushNamed(context,RegistrationScreen.id);
+                    
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Sign Up ',
+                  ),
+                )
+              ),
+            ),
+            
+            
+           
+          ],
+        ),
+        
+      ),
+      
+    );
+  }
+}
