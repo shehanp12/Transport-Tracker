0b2cb49d6cfdba745efb29206a227967f33f5ad6
shehan <shehanp068@gmail.com>
2020-04-18 12:28:19 +0530

    sds

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flash_chat/screens/driver_screens/LoginScreen.dart';
-import 'package:flash_chat/screens/driver_screens/Registation_Screen.dart';
+
 import 'package:flash_chat/screens/driver_screens/welcome_screen.dart';
 import 'package:flash_chat/screens/driver_screens/driver_map_screen.dart';
 import 'package:flash_chat/screens/wrapper.dart';
 import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/driver_screens/welcome_screen.dart';
+
 import 'package:flash_chat/screens/driver_screens/login_screen.dart';
 import 'package:flash_chat/screens/driver_screens/registration_screen.dart';
 import 'package:flash_chat/screens/home_screen.dart';
