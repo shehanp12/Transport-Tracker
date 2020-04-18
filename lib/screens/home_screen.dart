d3598415e6cefb5faa9b9813b46928842153d5be
Anjana-lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2020-04-18 12:10:39 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,11 +12,15 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Transport Tracker'), 
-      ),
-        body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blueAccent, Colors.lightBlueAccent]
+            )
+          ),
+        
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -26,9 +30,11 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
                 elevation: 5.0,
-                color: Colors.lightBlueAccent,
+                color: Colors.white,
                 borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
+                child: Container(
+                  
+                  child:MaterialButton(
                   onPressed: (){
                      Navigator.of(context).pushNamed( Wrapper.id);
                   },
@@ -38,6 +44,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     'Driver ',
                   ),
                 )
+                ),
               ),
             ),
              Padding(
