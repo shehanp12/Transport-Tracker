886f1487c838193fec312540337031c44c7129e0
Anjana-lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2020-05-07 21:53:54 +0530

    sign up Ui

--- lib/screens/user_screens/map_student.dart
+++ lib/screens/user_screens/map_student.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:typed_data';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
