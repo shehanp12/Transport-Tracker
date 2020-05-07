af3fd6c9762da554edd93d09a1b5b362ea53c579
Anjana-lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2020-05-07 11:11:27 +0530

    log in ui

--- lib/screens/driver_screens/login_screen.dart
+++ lib/screens/driver_screens/login_screen.dart
@@ -21,17 +21,35 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-        title: Text('Sign In')
-      ),
+      resizeToAvoidBottomInset: false,
       body: Container(
+        
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blue[50], Colors.lightBlueAccent]
+            )
+          ),
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
         child: Form(
           key:_formKey,
           child: Column(
             children: <Widget>[
+               Padding(padding: EdgeInsets.all(50.0)), 
               SizedBox(height: 20),
               TextFormField(
+                decoration:  InputDecoration(
+                        labelText: "Enter an email address",
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: BorderSide(
+                            
+                          ),
+                        ),
+                        //fillColor: Colors.green
+                      ),
                 validator:(val)=> val.isEmpty ? 'Enter an email address':null,
                 
                 onChanged: (val) {
@@ -42,6 +60,17 @@ class _LoginScreenState extends State<LoginScreen> {
               }),
               SizedBox(height: 20),
               TextFormField(
+                decoration:  InputDecoration(
+                        labelText: "Enter password",
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: BorderSide(
+                            
+                          ),
+                        ),
+                        //fillColor: Colors.green
+                      ),
                  validator:(val)=> val.length<6 ? 'Enter an password 6+ chars long ':null,
                 
                 obscureText: true,
@@ -53,12 +82,34 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
                   SizedBox(height: 40),
               RaisedButton(
-                color:Colors.blueAccent,
-                child: Text(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                  padding: const EdgeInsets.all(0.0),
+                  child: Ink(
+                  decoration: BoxDecoration(
+                  boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300],
+              blurRadius: 10.0, // has the effect of softening the shadow
+              spreadRadius: 1.0, // has the effect of extending the shadow
+              offset: Offset(
+                5.0, // horizontal, move right 10
+                5.0, // vertical, move down 10
+              ),
+            ),
+          ],
+          color: Colors.white38,
+          borderRadius: BorderRadius.circular(30),
+      
+               ),
+             child: Container(
+              constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0), // min sizes for Material buttons
+              alignment: Alignment.center,
+              child: const Text(
                   'Sign In',
-                  style:TextStyle(color:Colors.white),
-
-                ),
+              textAlign: TextAlign.center,
+                    ),
+                   ),
+                  ),
                 onPressed: () async{
                   
                   if(_formKey.currentState.validate()){
